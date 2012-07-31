834f58450693e772e9306349b6bf256146f664d9
morhys <morhys.m@googlemail.com>
2012-07-31 12:04:33 +0100

    first

---
+++ init.rb
@@ -0,0 +1,13 @@
+#### Food Finder ####
+
+APP_ROOT = File.dirname(__FILE__)
+
+# require "#{APP_ROOT}/lib/guide"
+# require File.join(APP_ROOT, 'lib', 'guide.rb')
+# require File.join(APP_ROOT, 'lib', 'guide')
+
+$:.unshift( File.join(APP_ROOT, 'lib') )
+require 'guide'
+
+guide = Guide.new('restaurants.txt')
+guide.launch!
